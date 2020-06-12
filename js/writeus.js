04a50b39893567adff69e7829a930691f2d84c1a
Vafin Artem <a.vafin85@gmail.com>
2020-06-12 10:32:26 +0500

    настроили фокус в форме напиши нам

--- js/writeus.js
+++ js/writeus.js
@@ -1,12 +1,47 @@
 let writeUsButton = document.querySelector(".writeus-btn")
-let wrirteUsPopup = document.querySelector(".popup-writeus")
-let writeUsClose = document.querySelectorAll(".close-btn")
+let writeUsPopup = document.querySelector(".popup-writeus")
+let writeUsCloses = document.querySelectorAll(".close-btn")
+let writeUsForm = writeUsPopup.querySelector(".write-us-form");
+let writeUsName = writeUsPopup.querySelector(".write-us-name")
+let writeUsEmail = writeUsPopup.querySelector(".writeus-email")
+let writeUsMessage = writeUsPopup.querySelector(".write-us-message")
+
+let isStorageSupport = true;
+let storageName = "";
+let storageEmail = "";
+
+try {
+    storageName = localStorage.getItem("name");
+    storageEmail = localStorage.getItem("email");
+} catch (err) {
+    isStorageSupport = false;
+}
 
 
 writeUsButton.addEventListener("click", function() {
-    wrirteUsPopup.classList.remove("popup");
+    writeUsPopup.classList.remove("popup");
+
+    if (storageName) {
+        writeUsName.value = storageName;
+        writeUsEmail.focus();
+        writeUsEmail.value = storageEmail;
+        writeUsMessage.focus();
+    } else {
+        writeUsName.focus();
+    }
 });
 
-writeUsClose.addEventListener("click", function() {
-    wrirteUsPopup.classList.add("popup");
+writeUsCloses[1].addEventListener("click", function() {
+    writeUsPopup.classList.add("popup");
+});
+
+writeUsForm.addEventListener("submit", function(evt) {
+    if (!writeUsName.value || !writeUsEmail.value || !writeUsMessage.value) {
+        evt.preventDefault();
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("name", writeUsName.value);
+            localStorage.setItem("email", writeUsEmail.value);
+        }
+    }
 });
