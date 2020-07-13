816b09d20caf7eee260443ad21cd038cd119fa28
Vafin Artem <a.vafin85@gmail.com>
2020-07-13 23:52:44 +0500

    тестируем и усовершенствуем сладйер

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,8 @@
 let sliderToggle = document.querySelectorAll(".slider-toggle");
 let sliderItem = document.querySelectorAll(".slider-item");
+let next = document.querySelector(".next")
+let prev = document.querySelector(".prev")
+
 
 for (let i = 0; i < sliderToggle.length; i++) {
     sliderToggle[i].addEventListener("click", function() {
@@ -10,5 +13,15 @@ for (let i = 0; i < sliderToggle.length; i++) {
         let sliderCurrent = document.querySelector(".slide-active");
         sliderCurrent.classList.remove("slide-active");
         sliderItem[i].classList.add("slide-active");
+    });
+
+    next.addEventListener("click", function() {
+        let sliderCurrent = document.querySelector(".slide-active");
+        sliderCurrent.classList.remove("slide-active");
+        sliderItem[i].classList.add("slide-active");
+        let toggleCurrent = document.querySelector(".current");
+        toggleCurrent.classList.remove("current");
+        sliderToggle[i].classList.add("current");
+
     });
 }
